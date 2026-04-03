39276eba5be43e2beb4bbbcbf4ef2d2d6e379e6f
AToughKid <Jirawat.somro@ku.th>
2026-04-03 16:08:31 +0700

    [round_4] api test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import magicalSvg from 'vite-plugin-magical-svg'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    magicalSvg({
+      target: 'react' // ตั้งค่าให้แปลงเป็น React Component
+    })
+  ],
   test: {
     globals: true,                          
     environment: 'jsdom',                   
-    setupFiles: './src/setupTests.js',      
+    setupFiles: './src/setupTests.js',     
   },
 })
